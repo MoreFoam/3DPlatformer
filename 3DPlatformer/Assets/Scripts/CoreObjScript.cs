16c796a9df1957ded87133822140fa43902a220c
twoAcestwo8s <twoAcestwo8s@gmail.com>
2016-09-19 21:56:41 -0500

    Added platformer movement and camera control, jump needs to be fixed

---
+++ 3DPlatformer/Assets/Scripts/CoreObjScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoreObjScript : MonoBehaviour {
+
+	private GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.Find("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position;
+	
+	}
+}
