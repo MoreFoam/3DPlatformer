7c599494e1cc6d742300bc65a8b3db721a0ee11a
cokeman5 <cokeman511@gmail.com>
2016-10-19 22:10:55 -0500

    Everybody's part except Tim's

---
+++ 3DPlatformer/Assets/Scripts/HubLoad.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.SceneManagement;
+
+public class HubLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider Col)
+	{
+		//Make sure the player object has its tag set to Player
+		if (Col.CompareTag("Player"))
+		{
+			Application.LoadLevel("Level 1");
+		}
+	}
+}
