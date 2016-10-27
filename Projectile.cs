46e3bffbb65f75ddcc95f53fe664de4d33abf95a
Paul Cho <paul.cho@ttu.edu>
2016-10-26 21:17:26 -0500

    Give Player the ability shoot projectiles
    1. Locate the Player in Scene
    2. GameObject -> Create Empty
    3. Place the object in front and center of the character's face.
    4. Call this object 'spawn'
    5. Attach spawn to Player->Camera
    6. Add Projectile script to Player
    7. Drag fireball.prefab to Projectile under Projectile Script
    8. Drag spawn to Spawn under Projectile Script

---
+++ Projectile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform spawn;
+    public int destroyTime = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody clone;
+            clone = (Rigidbody)Instantiate(projectile, spawn.position, projectile.rotation);
+
+            clone.velocity = spawn.TransformDirection(Vector3.forward * 30);
+            Destroy(clone.gameObject, destroyTime);
+        }
+    }
+}
