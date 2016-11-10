46dad3771bde131e9cd1aa2c1987c39d5f6b32a9
Paul Cho <paul.cho@ttu.edu>
2016-11-09 19:49:45 -0600

    Collision Detection for Projectiles
    Add ProjectileCollision.cs to fireball object used in Projectile script.
    When a fireball collides with an object tagged as "Enemy", it will be destroyed.
    Created and added the tag "Enemy" to the object Ant_AI_Model_Legs_Combined.

---
+++ ProjectileCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
