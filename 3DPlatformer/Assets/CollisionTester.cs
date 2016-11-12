b8d90e8a5359a6180258cc51827fbaa7037300fb
twoAcestwo8s <twoAcestwo8s@gmail.com>
2016-11-12 17:59:04 -0600

    Added basis for health system

---
+++ 3DPlatformer/Assets/CollisionTester.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter(Collision Col)
+	{
+		if (Col.gameObject.tag == "Player");
+		Debug.Log("YO GET YO HANDS OFF ME FAM!");
+	}
+}
---
+++ 3DPlatformer/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthSystem : MonoBehaviour {
+
+	private int health;
+	private int maxHealth;
+	private bool isHit = false;
+
+	// Use this for initialization
+	void Start () {
+		maxHealth = 3;
+		health = maxHealth;
+	
+	}
+		
+	IEnumerator waitUpSon(){
+		print("Hey man chill out!");
+		yield return new WaitForSeconds(5);
+	}
+
+	void OnCollisionEnter(Collision Col){
+
+		if (Col.gameObject.tag == "Enemy"){
+			if (isHit){
+				Debug.Log("You were previously hit!");
+				StartCoroutine(waitUpSon());
+				isHit = false;
+			}
+			else if (health > 0){
+
+				if (health <= maxHealth){
+					health = health - 1;
+				}
+				Debug.Log("YOU WERE HIT! Current health: " + health.ToString());
+				isHit = true;
+			}
+		}
+
+	}
+}
