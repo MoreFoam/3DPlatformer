6e273b4b7e062a469f7376bd6d1d0e31532e465f
Paul Cho <paul.cho@ttu.edu>
2016-11-13 19:59:17 -0600

    Adding a Sound Effect When Destroying an Enemy
    1. Select Fireball object that is being used for projectile
    2. Add Component -> Audio Source
    3. Drag the audio clip Boop.mp3 into AudioClip
    4. Update script for ProjectileCollision.cs
    5. Drag the audio clip Boop.mp3 into Hit (ProjectileCollision script)

--- ProjectileCollision.cs
+++ ProjectileCollision.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class ProjectileCollision : MonoBehaviour {
 
+    public AudioClip hit;
+
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Enemy")
         {
+            GetComponent<AudioSource>().PlayOneShot(hit);
             Destroy(col.gameObject);
         }
     }
