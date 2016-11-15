25a132f0eae40a34cd66cf24b63eb51f9510f6ae
Adrian1000 <adrian.palmerola@gmail.com>
2016-11-14 23:28:16 -0600

    Added items in
    Missing: Interaction with plate and chest. Outside of that plate is
    usable to create all interactions with doors and such.

---
+++ 3DPlatformer/Assets/Models&Animations/FPSController/Fps.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+#pragma strict
+public class Fps : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+        Application.targetFrameRate = 300;
+        QualitySettings.vSyncCount = 0;
+    }
+}
---
+++ 3DPlatformer/Assets/Models&Animations/Treasure_box/Tresure_box/chest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chest : MonoBehaviour
+{
+    public AudioClip Jingle;
+    private Animator anim;
+    private AudioSource ChestopenJingle;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        ChestopenJingle = GetComponent<AudioSource>();
+    }
+
+    public void chestToggle()
+    {
+        if (anim)
+        {
+            if (anim.GetBool("isClosed"))
+            {
+                anim.SetBool("isClosed", false);
+                anim.SetBool("isOpen", true);
+                float vol = Random.Range(volLowRange, volHighRange);
+                ChestopenJingle.PlayOneShot(Jingle, vol);
+
+            }
+            else if (anim.GetBool("isOpen"))
+            {
+                anim.SetBool("isOpen", false);
+                anim.SetBool("isClosed", true);
+            }
+        }
+
+    }
+}
