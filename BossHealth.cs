ce12522882a72563f24ebc744bce5a1054bf0fe0
Paul Cho <paul.cho@ttu.edu>
2016-11-30 21:12:12 -0600

    Adding Ant Boss with Health (10 HP)
    1. Duplicate Ant_AI
    2. Rename 'Ant_Boss'
    3. Remove Enemy Tag
    4. Ant_Boss -> Transform -> Position -> x=181.1, y=38.3, z=288.1
    5. Ant_Boss -> Ant_AI_Anim_Resize -> Transform -> Scale -> x=70, y=70, z=130
    6. Go to Ant_Boss -> Ant_AI_Anim_Resize
    7. Add Component -> BossHealth script -> Drag 'Boop' into Hit
    8. Add Component -> Audio Source -> Drag 'Boop' into AudioClip
    9. Locate object being used as fireball -> Add the tag "fireball"

---
+++ BossHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossHealth : MonoBehaviour {
+
+    public int life = 0;
+    public AudioClip hit;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "fireball")
+        {
+            life += 1;
+            GetComponent<AudioSource>().PlayOneShot(hit);
+            if(life == 10)
+                Destroy(gameObject);
+
+        }
+    }
+}
