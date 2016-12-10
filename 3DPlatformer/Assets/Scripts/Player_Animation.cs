4db4200d037128930ad56defbb01bb1cd2189103
MoreFoam <chris.marler@me.com>
2016-12-09 23:35:50 -0600

    added portal models for hubworld

---
+++ 3DPlatformer/Assets/Scripts/Player_Animation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Animation : MonoBehaviour {
+
+    private Animation anim;
+    private int c = 0;
+
+    void Awake() {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            anim.Play("Player_Walk");
+        else
+            anim.Stop();
+    }
+}
